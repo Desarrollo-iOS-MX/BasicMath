67da11352ccedbcc48e71e2beac2832b6f0a2de0
Miguel Torres <99153283+MrPicudo@users.noreply.github.com>
2025-10-22 05:34:58 -0600

    Initial Commit

---
+++ BasicMath/BasicMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicMathApp.swift
+//  BasicMath
+//
+//  Created by José Miguel Torres Chávez Nava on 22/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
